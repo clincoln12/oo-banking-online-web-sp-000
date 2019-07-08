8af59a5de59759617ba70e9634a8856a45329863
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-08 17:09:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   attr_reader :name
   attr_accessor :status, :balance
   
-  def self.initialize(name)
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = open
