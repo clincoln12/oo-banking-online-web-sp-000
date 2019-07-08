9ae93667b1ff43680d448e6d5e10d9f9f9611825
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-08 17:27:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
   
   def valid?
-    return true if @sender.valid? && @receiver.valid? && @status == "pending
+    return true if @sender.valid? && @receiver.valid?
     false
   end
   
